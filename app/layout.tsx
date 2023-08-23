575fd3083ef1dde010a1b54eb1b97a814a4d4a36
Yashveer Singh <yashveer.rtc@gmail.com>
2023-08-23 09:31:24 +0530

    completed footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Footer, Navbar } from "@/components";
 import "./globals.css";
 import type { Metadata } from "next";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Car Hub",
   description: "Discover the best cars in the world",
 };
