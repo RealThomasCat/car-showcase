e1fb56f7a5b8fd09aced7753784ebc37eaba68ad
Yashveer Singh <yashveer.rtc@gmail.com>
2023-08-28 15:29:18 +0530

    implemented car card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@utils";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  // console.log(allCars);
+
+  // Array.isArray is a built-in JavaScript/TypeScript function that is used to check if a given value is an array or not.
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -10,16 +18,30 @@ export default function Home() {
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
+        </div>
 
-          <div className="home__filters">
-            <SearchBar />
+        <div className="home__filters">
+          <SearchBar />
 
-            <div className="home__filter-container">
-              <CustomFilter title="fuel" />
-              <CustomFilter title="year" />
-            </div>
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+          </div>
+        )}
       </div>
     </main>
   );
